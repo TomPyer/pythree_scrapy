9cb92b59a3a4c8e300c28462bb926db5b9173290
Tangxl <jizhidetangxuelin@outlook.com>
2017-12-12 14:40:33 +0800

    适应articleSpider爬虫的启动文件

--- dynamicSpider/coderun.py
+++ dynamicSpider/coderun.py
@@ -2,28 +2,37 @@
 """
 Topic: 动态配置爬虫-脚本启动多爬虫
 """
-import scrapy
+
+import logging
+from dynamicSpider.articleSpider import ArticleSpider
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
 from scrapy.utils.log import configure_logging
+from dynamicSpider.models import db_connect
+from dynamicSpider.models import ArticleRule
+from sqlalchemy.orm import sessionmaker
 
+if __name__ == '__main__':
+    settings = get_project_settings()
+    configure_logging(settings)
+    db = db_connect()
+    Session = sessionmaker(bind=db)
+    session = Session()
+    rules = session.query(ArticleRule).filter(ArticleRule.enable == 1).all()
+    session.close()
+    runner = CrawlerRunner(settings)
 
-class MySpider1(scrapy.Spider):
-    # Your first spider definition
-    ...
+    for rule in rules:
+        # stop reactor when spider closes
+        # runner.signals.connect(spider_closing, signal=signals.spider_closed)
+        runner.crawl(ArticleSpider, rule=rule)
 
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
 
-class MySpider2(scrapy.Spider):
-    # Your second spider definition
-    ...
+    # blocks process so always keep as the last statement
+    reactor.run()
+    logging.info('all finished.')
 
-
-configure_logging()
-runner = CrawlerRunner()
-runner.crawl(MySpider1)
-runner.crawl(MySpider2)
-d = runner.join()
-d.addBoth(lambda _: reactor.stop())
-
-reactor.run()  # the script will block here until all crawling jobs are finished
 
